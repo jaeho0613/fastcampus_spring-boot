154705b609dcbf80767d667570727e1bb51404dd
Jaeho Jeong <ywm2004@gmail.com>
2021-05-14 17:24:35 +0900

    * [ Model 클래스 작성 완료 ]

---
+++ TodoServer/src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "todoOrder", nullable = false)
+    private Long order;
+
+    @Column(nullable = false)
+    private Boolean complete;
+}
