accc4b7e609b957ba25ce27a22e564420d411c6d
Jaeho Jeong <ywm2004@gmail.com>
2021-05-27 20:19:25 +0900

    [ Delete 강좌 완료 ]

---
+++ RestApiProject/src/main/java/com/example/Controller/DeleteApiController.java
@@ -0,0 +1,14 @@
+package com.example.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/delete")
+public class DeleteApiController {
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account) {
+        System.out.println(userId);
+        System.out.println(account);
+    }
+}
