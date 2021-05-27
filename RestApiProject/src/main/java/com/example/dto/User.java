ade9b60b40dc0c254f835e56b8ca19c63a32afe9
Jaeho Jeong <ywm2004@gmail.com>
2021-05-27 20:53:26 +0900

    [ Object Mapper 강좌 완료 ]

--- RestApiProject/src/main/java/com/example/dto/User.java
+++ RestApiProject/src/main/java/com/example/dto/User.java
@@ -2,17 +2,24 @@ package com.example.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL) // Null 값 제외
 public class User {
 
     private String name;
     private Integer age;
+
+    @JsonProperty("phone_number")
     private String phoneNumber;
     private String address;
 
