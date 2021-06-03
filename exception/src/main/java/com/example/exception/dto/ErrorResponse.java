47b4240b7504b43ad640a0dc17fe10a10295d332
Jaeho Jeong <ywm2004@gmail.com>
2021-06-03 15:23:23 +0900

    ** [ Exception Validation 처리 ] - Json return 형태 - 각 Exception에 따른 dto 객체 - total error를 한곳에서 관리할 수 있는 방식

---
+++ exception/src/main/java/com/example/exception/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.exception.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ErrorResponse {
+
+    String statusCode;
+    String requestUrl;
+    String code;
+    String message;
+    String resultCode;
+
+    List<Error> errorList;
+
+}
