bc4ecb9eece949850fc9e0cf0d107f748e5713c6
jaeho0613 <ywm2004@gmail.com>
2022-03-15 17:59:33 +0900

    [ 서버 설정 변경 ]

--- part04/src/main/resources/data.sql
+++ part04/src/main/resources/data.sql
@@ -1,5 +1,10 @@
-insert into user (id, name, email, created_at, updated_at) values ( 1, 'jaeho', 'jaeho@gmail.com', now(), now() );
-insert into user (id, name, email, created_at, updated_at) values ( 2, 'jaeho', 'jaeho@naver.com', now(), now() );
-insert into user (id, name, email, created_at, updated_at) values ( 3, 'minsu', 'minsu@gmail.com', now(), now() );
-insert into user (id, name, email, created_at, updated_at) values ( 4, 'coco', 'coco@gmail.com', now(), now() );
-insert into user (id, name, email, created_at, updated_at) values ( 5, 'somang', 'somang@gmail.com', now(), now() );
+insert into user (id, name, email, gender, created_at, updated_at)
+values (1, 'jaeho', 'jaeho@gmail.com', 'MALE', now(), now());
+insert into user (id, name, email, gender, created_at, updated_at)
+values (2, 'jaeho', 'jaeho@naver.com', 'MALE', now(), now());
+insert into user (id, name, email, gender, created_at, updated_at)
+values (3, 'minsu', 'minsu@gmail.com', 'FEMALE', now(), now());
+insert into user (id, name, email, gender, created_at, updated_at)
+values (4, 'coco', 'coco@gmail.com', 'FEMALE', now(), now());
+insert into user (id, name, email, gender, created_at, updated_at)
+values (5, 'somang', 'somang@gmail.com', 'FEMALE', now(), now());
